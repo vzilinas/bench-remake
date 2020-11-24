5abaacea3d759072298769277ccf99270252029c
vzilinas <vzilinas@users.noreply.github.com>
2020-11-24 18:27:48 +0200

    Update submit_heron.sh

--- submit_heron.sh
+++ submit_heron.sh
@@ -1,2 +1,2 @@
 heron submit local compiled/benchmark-topology.jar org.vz.benchmark.AdvertisementTopology AdvertisementTopology  --deploy-deactivated
-# heron submit local compiled/benchmark-topology.jar org.vz.benchmark.WordCountTopology WordCountTopology  --deploy-deactivated
+heron submit local compiled/benchmark-topology.jar org.vz.benchmark.WordCountTopology WordCountTopology  --deploy-deactivated
